154bcbafe730f4f7d3b2387fd2cb65ec28a83098
yverhenne <129364360+yverhenne@users.noreply.github.com>
2025-08-07 19:02:00 +0200

    Delete plugin/user_profile directory

--- plugin/user_profile/user_profile/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__.'/config.php';
-require_once __DIR__.'/UserProfilePlugin.php';
-
-if (!api_is_platform_admin()) {
-    exit('You must have admin permissions to uninstall plugins');
-}
-UserProfilePlugin::create()->uninstall();
-?>
